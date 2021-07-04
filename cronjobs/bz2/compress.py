de85d75aab403539e5970fba685121fddf4a29a1
gabyflea <gaby.santamaria@outlook.fr>
2021-07-04 12:40:03 +0200

    adding timestamp verification and extension mask

--- cronjobs/bz2/compress.py
+++ cronjobs/bz2/compress.py
@@ -2,24 +2,51 @@
 
 # Namecheap Python version: 2.6.6 (WTF IT SUCKS)
 import os
+import sys
 import bz2
-import glob
 
-ignore = [
+import time
+
+ignore = [ # files to ignore
     __file__,
     'robot.txt',
     '.htaccess',
-    '.ftpquota'
+    '.ftpquota',
+    '.gitignore'
 ]
+
+ext_ignore = [ # file extensions that shouldn't be compressed
+    '.html',
+    '.css',
+    '.js',
+    '.py',
+    '.bz2'
+]
+
+filename = 'last_compression'
+delay = 24 * 3600
+
+if os.path.exists(filename):
+    print('[compress] Checking latest compression timestamp...')
+    with open(filename, 'r') as f:
+        if float(f.readline()) - time.time() <= delay:
+            sys.exit('[compress] Aborting file compression.')
+else:
+    with open(filename, 'w') as f:
+        f.write(str(time.time()))
+
 
 print('[compress] Starting to compress files...')
 
 for root, subdirs, files in os.walk('fastdl.enragedgamers.net'):
     for file in files:
         if file in ignore: continue
+        if os.path.splitext(file)[1] in ext_ignore: continue
+
         path = os.path.join(root, file)
-        if os.path.splitext(file)[1] == '.bz2': continue
+        
         print('[compress] Compressing {0}...'.format(file))
+
         with open(path, 'rb+') as f: # compress the binary data and write to a new file
             new = path + '.bz2'
             compress = bz2.compress(f.read())
@@ -28,3 +55,4 @@ for root, subdirs, files in os.walk('fastdl.enragedgamers.net'):
                     os.utime(new, None)
             with open(new, 'wb') as n:
                 n.write(compress)
+ 
